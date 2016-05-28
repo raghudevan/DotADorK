ccef7ab36f9799dbf820b19ba7c233709adc6f9c
Raghudevan S <raghudevan.s@mu-sigma.com>
2016-05-29 00:22:06 +0530

    Bug
    * Changed PlayerConnect from a stateless function -> a class
    * config publicpath was wrong

--- src/components/PlayerConnect.js
+++ src/components/PlayerConnect.js
@@ -1,18 +1,27 @@
 import React from 'react';
 
-const PlayerConnect = (props) => {
-	return (
-		<div>
-			<input onKeyUp={onKeyUp.bind(null, props.doConnect)}
-			type="text" placeholder="Input your user handle" autoFocus/>
-			<button onClick={props.doConnect}>Connect</button>
-		</div> 
-	);
-}
+class PlayerConnect extends React.Component {
+	onKeyUp = (e) => {
+		if(e.which === 13) {
+			onClick();
+		}
+	}
 
-function onKeyUp(doConnect, e) {
-	if(e.which === 13) {
-		doConnect(e.currentTarget.value);
+	onClick = () => {
+		this.props.doConnect(this.refs.playername.value)
+	}
+
+	render() {
+		return (
+			<div>
+				<input onKeyUp={this.onKeyUp}
+				ref="playername" type="text" 
+				placeholder="Input your user handle" autoFocus/>
+				<button onClick={this.onClick}>
+					Connect
+				</button>
+			</div> 
+		);
 	}
 }
 
